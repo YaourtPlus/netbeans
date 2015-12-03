690500b2ddd67d9469f1f75dbdbd7fc68702e433
snoopydesbois <tbenoist@BE14-18.ens-sfa.univ-poitiers.fr>
2015-12-03 10:22:15 +0100

    Ajout NotificationsEntity/DAO

---
+++ YaourtPlus/src/java/DAO/NotificationsDAO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author tbenoist
+ */
+public interface NotificationsDAO {
+		public void save(NotificationsEntity n);
+		public void update(NotificationsEntity n);
+		public void delete(NotificationsEntity n);
+		public NotificationsEntity find(int id);
+		public List<NotificationsEntity> findAll();
+		public List<NotificationsEntity> findByNotifieur(int notifieurId);
+		public List<NotificationsEntity> findByDate(Date date);
+		public List<NotificationsEntity> findByDestinataire(int destinataireId);
+}
