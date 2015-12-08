a6cc6bab35444762e61c4e4d226f407ef9048fec
snoopydesbois <tbenoist@BE14-15.ens-sfa.univ-poitiers.fr>
2015-12-08 18:49:21 +0100

    Ajout de l'affichage du mur après la connexion

--- YaourtPlus/src/java/Controllers/MurController.java
+++ YaourtPlus/src/java/Controllers/MurController.java
@@ -1,8 +1,11 @@
 package Controllers;
 
+import Service.ConnexionService;
 import Service.MurService;
+import Service.ProfilService;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,18 +26,44 @@ public class MurController {
 
     @Autowired
     MurService murService;
+	
+	@Autowired
+	ProfilService profilService;
 
+    @Autowired
+    ConnexionService connexionService;
     
 
     @RequestMapping(value = "mur", method = RequestMethod.POST)
     public ModelAndView handleRequestInternal(HttpServletRequest request,
             HttpServletResponse response) throws Exception {
-        String result;
-        ModelAndView mv = new ModelAndView("mur");
-        String nom = request.getParameter("nom");
-
-        result = "test";
-        mv.addObject("murMessage", result);
+        ModelAndView mv;
+		String login = request.getParameter("login");
+		String password = request.getParameter("password");
+		
+		// Création de la session
+		HttpSession session = request.getSession(true); 
+		
+		if(session != null){
+			
+			session.setAttribute("idPersonne", connexionService.connexion(login, password));
+			int idPersonne = (int) session.getAttribute("idPersonne");
+			if(idPersonne != -1){
+				mv  = new ModelAndView("mur");
+				String nomPersonne = profilService.getPersonne(idPersonne).getNom();
+				String mur = "";
+				mv.addObject("nomPersonne", nomPersonne);
+				mv.addObject("murMessage", mur);
+			}
+			else{	
+				mv = new ModelAndView("connexion");
+				mv.addObject("inscriptionMessage", "Login ou mot de passe incorrect");
+			}
+		}
+		else{
+			mv = new ModelAndView("connexion");
+			mv.addObject("inscriptionMessage", "Login ou mot de passe incorrect");
+		}
         return mv;
     }
 }
